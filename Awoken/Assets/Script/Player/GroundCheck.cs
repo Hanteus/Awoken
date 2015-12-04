a1dc5dfbeca29e5ee1d652edae3107a9232bd733
Marco <marco.ballabio@outlook.it>
2015-12-04 11:22:22 +0100

    Aggiunte cose

--- Awoken/Assets/Script/Player/GroundCheck.cs
+++ Awoken/Assets/Script/Player/GroundCheck.cs
@@ -10,34 +10,31 @@ public class GroundCheck : MonoBehaviour {
     }
 
     void OnTriggerEnter2D(Collider2D c) {
-
         if ( c.gameObject.tag == "Ground" ) {
             player.getPlayerAnim ().SetBool ( "Grounded" , true );
+        }
+        if (c.gameObject.tag == "Mobile Platform")
+        {
+            Debug.Log("IN");
+            player.getPlayerAnim().SetBool("Grounded", true);
+            transform.parent.parent = c.transform.parent;
         }
     }
 
     void OnTriggerStay2D ( Collider2D c ) {
         if ( c.gameObject.tag == "Ground" ) {
             player.getPlayerAnim ().SetBool ( "Grounded" , true );
-        }
-
-        /*
-            else if (c.gameObject.tag == "Platform") {
-                player.getPlayerAnim().SetBool("Grounded", true);
-                transform.parent.parent = c.transform;
-            }
-        */
+        } 
     }
 
     void OnTriggerExit2D(Collider2D c) {
         if ( c.gameObject.tag == "Ground" ) {
             player.getPlayerAnim ().SetBool ( "Grounded" , false );
         }
-
-        /*
-            else if (c.gameObject.tag == "Platform") {
-                transform.parent.parent = null;
-        }*/
+        if (c.gameObject.tag == "Mobile Platform") {
+            Debug.Log("OUT");
+            transform.parent.parent = null;
+        }
     }
 
 }

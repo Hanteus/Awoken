4e98ab937f0633535f0b9bdc4c3715d1c8392815
Giuseppe Fabio Trentadue <giuseppefabio.trentadue@studenti.unimi.it>
2015-12-13 17:56:43 +0100

    Terminata EnemyAI

--- Awoken/Assets/Script/Enemy.cs
+++ Awoken/Assets/Script/Enemy.cs
@@ -1,61 +1,139 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class Enemy : MonoBehaviour {
 
     public float walkSpeed = 1.0f;      // Walkspeed
     public float wallLeft;       // Define wallLeft
     public float wallRight;      // Define wallRight
-    float walkingDirection;
-    Vector2 walkAmount;
-    Vector3 rotate;
-    float originalX; // Original float value
 
+    private Vector2 walkAmount;
+    private Vector3 rotateAmount;
+    private Vector3 originalPos; // Original float value
+
+    private bool attackCheck = false;
+    private bool patroling = true;
+    private bool facingRight;
+
+    private float attackDelta = 2.0f;
+    private float distanceToPlayer;
+    private float originDelta = 0.5f;
+    private float distanceToOrigin;
+
+    private Transform player;
+    
     void Start () {
-        this.originalX = this.transform.position.x;
+        this.originalPos = this.transform.position;
 
-        wallLeft = transform.position.x - 2.5f;
-        wallRight = transform.position.x + 2.5f;
+        wallLeft = transform.position.x - 3.5f;
+        wallRight = transform.position.x + 3.5f;
 
-        Debug.Log ( "Position: " + originalX + " WallLeft: " + wallLeft + " WallRight: " + wallRight );
+        Debug.Log ( "Position: " + originalPos.x + " WallLeft: " + wallLeft + " WallRight: " + wallRight );
 
-        /*if (this.transform.rotation.eulerAngles.y < 180 ) {
-            //moves left
-            walkingDirection = -1.0f;
+        player = GameObject.FindGameObjectWithTag ( "Player" ).transform;
+
+        if ( this.transform.rotation.eulerAngles.y < 180 ) {
+            facingRight = false;
         }
-        else{
-            //moves right
-            walkingDirection = 1.0f;
-        }*/
-            
-    }
+        else if ( this.transform.rotation.eulerAngles.y >= 180 ) {
+            facingRight = true;
+        }
 
-    // Update is called once per frame
+    }
+    
     void Update () {
-        walkAmount.x = - walkSpeed * Time.deltaTime;
+        //Evaluate distance to originalPos
+        distanceToOrigin = Vector2.Distance ( this.transform.position , originalPos );
 
-        Debug.Log ( "WA " + walkAmount.x );
+        //Check for the player position in range
+        distanceToPlayer = Vector2.Distance ( this.transform.position , player.transform.position );
+        
 
-        if (this.transform.position.x >= wallRight ) {
-            //walkingDirection = -1.0f;
-            Debug.Log ( "1" );
-            Flip ( -1.0f );
+        if ( distanceToPlayer > 10) {
+            
+            if ( patroling ) {
+                //patroling
+                Debug.Log ( "Out of range" );
+
+                //Move the enemy back and forth
+                walkAmount.x = -walkSpeed * Time.deltaTime;
+
+                if ( this.transform.position.x >= wallRight ) {
+                    Debug.Log ( "Out of range, rotate to left" );
+                    rotateAmount.y -= 180.0f;
+                    this.transform.Rotate ( rotateAmount );
+                }
+                else if ( this.transform.position.x <= wallLeft ) {
+                    Debug.Log ( "Out of range, rotate to right" );
+                    rotateAmount.y += 180.0f;
+                    this.transform.Rotate ( rotateAmount );
+                }
+
+                this.transform.Translate ( walkAmount );
+            }
+            else {
+                //the player is escaped
+                //return to its position
+
+                Debug.Log ( "Carter escaped! Return to originalPos." );
+
+                if ( originalPos.x < this.transform.position.x ) {
+
+                    if ( this.transform.rotation.eulerAngles.y >= 180 ) {
+                        rotateAmount.y -= 180.0f;
+                        this.transform.Rotate ( rotateAmount );
+                    }
+                }
+                else if ( this.transform.position.x < originalPos.x ) {
+
+                    if ( this.transform.rotation.eulerAngles.y < 180 ) {
+                        rotateAmount.y += 180.0f;
+                        this.transform.Rotate ( rotateAmount );
+                    }
+                }
+
+                walkAmount.x = -walkSpeed * Time.deltaTime;
+
+                this.transform.Translate ( walkAmount );
+
+                if ( distanceToOrigin < originDelta ) {
+                    //restart to patrol
+                    patroling = true;
+                }
+            }
+
         }
-        else if (this.transform.position.x <= wallLeft ) {
-            Flip ( 1.0f );
-            Debug.Log ( "2" );
-            //walkingDirection = 1.0f;
-        }
+        else {
+            //reach the player and attack
+            patroling = false;
 
-        //Debug.Log ( "WD " + walkingDirection );
+            Debug.Log ( "In range" );
+            
+            if ( player.position.x < this.transform.position.x ) {
 
-        this.transform.Translate ( walkAmount );
-    }
+                if ( this.transform.rotation.eulerAngles.y >= 180 ) {
+                    Debug.Log ( "In range, rotate to left" );
+                    rotateAmount.y -= 180.0f;
+                    this.transform.Rotate ( rotateAmount );
+                }
+            }
+            else if ( this.transform.position.x < player.position.x ) {
 
-    void Flip ( float dir ) {
-        rotate.y += dir * 180f;
+                if ( this.transform.rotation.eulerAngles.y < 180 ) {
+                    Debug.Log ( "In range, rotate to right" );
+                    rotateAmount.y += 180.0f;
+                    this.transform.Rotate ( rotateAmount );
+                }
+            }
 
-        this.transform.Rotate ( rotate );
+            if ( distanceToPlayer <= attackDelta ) {
+                Debug.Log ( "Attack!!!" );
+            }
+            else {
+                walkAmount.x = -walkSpeed * Time.deltaTime;
+
+                this.transform.Translate ( walkAmount );
+            }
+        }       
     }
 
 }
